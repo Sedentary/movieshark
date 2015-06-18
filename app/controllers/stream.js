0444f664e43dfb1bdcc13418242409da7390b3e7
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-06-18 05:17:55 -0300

    examples converter video

--- app/controllers/stream.js
+++ app/controllers/stream.js
@@ -2,8 +2,11 @@
 
 'use strict';
 
-var fs = require('fs');
+var log = require('winston');
+var async = require('async');
 var torrentStream = require('torrent-stream');
+var ffmpeg = require('fluent-ffmpeg');
+var Transcoder = require('stream-transcoder');
 
 exports.index = function (req, res, next) {
     var magnet = req.url.replace('/?', '');
@@ -11,19 +14,25 @@ exports.index = function (req, res, next) {
     var engine = torrentStream(magnet);
 
     engine.on('ready', function() {
-        engine.files.forEach(function(file) {
-            console.log('filename:', file.name);
+
+        async.each(engine.files, function(file, cb) {
+            log.info('filename:', file.name);
 
             var total = file.length;
 
             // Get the filename
             var movieFileName = file.name;
             
-            var contentType = 'video/mp4';
+            var contentType = {
+                type: 'video/mp4',
+                convert: false
+            };
             if (movieFileName.indexOf('.ogg') !== -1) {
-                contentType = 'video/ogg';
+                contentType.type = 'video/ogg';
             } else if (movieFileName.indexOf('.webm') !== -1) {
-                contentType = 'video/webm';
+                contentType.type = 'video/webm';
+            } else if (movieFileName.indexOf('.avi') !== -1) {
+                contentType.convert = true;
             }
 
             // Chunks based streaming
@@ -41,20 +50,54 @@ exports.index = function (req, res, next) {
                 res.set('Content-Range',  'bytes ' + start + '-' + end + '/' + total);
                 res.set('Accept-Ranges', 'bytes');
                 res.set('Content-Length', chunksize);
-                res.set('Content-Type', contentType);
+                res.set('Content-Type', contentType.type);
                 // res.openedFile = file;
                 var stream = file.createReadStream({
                     start: start,
                     end: end
                 });
-                stream.pipe(res);
+
+                if (contentType.convert) {
+                    log.info('CONVERT');
+
+                    // new Transcoder(stream)
+                    //     .videoCodec('h264')
+                    //     .format('mp4')
+                    //     .videoBitrate(800 * 1000)
+                    //     .on('progress', function (progress) {
+                    //         log.info('progress: ', progress.progress);
+                    //     })
+                    //     .on('finish', function () {
+                    //         log.info('finished');
+                    //     })
+                    //     .stream().pipe(res);
+
+                    // ffmpeg(stream)
+                    //     .on('start', function(commandLine) {
+                    //         log.info('Started: ' + commandLine);
+                    //     })
+                    //     .on('progress', function(progress) {
+                    //         log.info('Processing: ' + progress.percent + '% done');
+                    //     })
+                    //     .on('error', function(err) {
+                    //         log.error(err.message);
+                    //     })
+                    //     .on('end', function() {
+                    //         lof.info('Processing finished!');
+                    //     })
+                    //     .output(res)
+                    //     .run();
+                } else {
+                    stream.pipe(res);
+                }
             } else {
                 res.status(200);
                 res.set('Content-Length', total);
-                res.set('Content-Type', contentType);
+                res.set('Content-Type', contentType.type);
                 // res.openedFile = file;
                 file.createReadStream().pipe(res);
             }
+            cb();
         });
     });
 }
