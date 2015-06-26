5dc45d48723e0871273bcca6af6ee343e50f3a78
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-06-26 03:13:25 -0300

    updated stream for get first file and engine.destroy in response close/finish

--- app/controllers/stream.js
+++ app/controllers/stream.js
@@ -21,63 +21,68 @@ exports.index = function (req, res) {
 
     engine.on('ready', function () {
 
-        engine.files.forEach(function (file) {
-            var movieFileName = file.name;
-            var extension = movieFileName.replace(/.*\./, '');
-            var contentType = null;
-            switch (extension) {
-                case 'ogg':
-                    contentType = 'video/ogg';
-                    break;
-                case 'webm':
-                    contentType = 'video/webm';
-                    break;
-                case 'mp4':
-                    contentType = 'video/mp4';
-                    break;
-            }
+        var file = engine.files[0];
+        var movieFileName = file.name;
+        var extension = movieFileName.replace(/.*\./, '');
+        var contentType = null;
+        switch (extension) {
+            case 'ogg':
+                contentType = 'video/ogg';
+                break;
+            case 'webm':
+                contentType = 'video/webm';
+                break;
+            case 'mp4':
+                contentType = 'video/mp4';
+                break;
+        }
 
-            if (!contentType)
-                return;
+        if (!contentType) {
+            engine.destroy();
+            return res.status(200).end();
+        }
 
-            log.info('filename:', file.name);
-            var total = file.length;
+        log.info('filename:', file.name);
+        var total = file.length;
 
-            // Chunks based streaming
-            if (req.headers.range) {
-                var range = req.headers.range;
-                var parts = range.replace(/bytes=/, "").split("-");
-                var partialstart = parts[0];
-                var partialend = parts[1];
+        // Chunks based streaming
+        if (req.headers.range) {
+            var range = req.headers.range;
+            var parts = range.replace(/bytes=/, "").split("-");
+            var partialstart = parts[0];
+            var partialend = parts[1];
 
-                var start = parseInt(partialstart, 10);
-                var end = partialend ? parseInt(partialend, 10) : total - 1;
-                var chunksize = (end - start) + 1;
+            var start = parseInt(partialstart, 10);
+            var end = partialend ? parseInt(partialend, 10) : total - 1;
+            var chunksize = (end - start) + 1;
 
-                res.status(206);
-                res.set('Content-Range', 'bytes ' + start + '-' + end + '/' + total);
-                res.set('Accept-Ranges', 'bytes');
-                res.set('Content-Length', chunksize);
-                res.set('Content-Type', contentType);
-                // res.openedFile = file;
-                var stream = file.createReadStream({
-                    start: start,
-                    end: end
-                });
+            res.status(206);
+            res.set('Content-Range', 'bytes ' + start + '-' + end + '/' + total);
+            res.set('Accept-Ranges', 'bytes');
+            res.set('Content-Length', chunksize);
+            res.set('Content-Type', contentType);
 
-                if (contentType.convert) {
-                    //TODO show error
-                } else {
-                    stream.pipe(res);
-                }
-            } else {
-                res.status(200);
-                res.set('Content-Length', total);
-                res.set('Content-Type', contentType);
-                // res.openedFile = file;
-                file.createReadStream().pipe(res);
-            }
+            var stream = file.createReadStream({
+                start: start,
+                end: end
+            });
+
+            stream.pipe(res);
+        } else {
+            res.status(200);
+            res.set('Content-Length', total);
+            res.set('Content-Type', contentType);
+            file.createReadStream().pipe(res);
+        }
+
+        res.on('close', function () {
+            engine.destroy();
         });
+
+        res.on('finish', function () {
+            engine.destroy();
+        });
+
     });
 
 };
