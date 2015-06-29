6b1653bd66cd815a88d97a8d598ea2f9ee13752f
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2015-06-28 21:27:05 -0300

    Search

---
+++ app/services/genre.js
@@ -0,0 +1,64 @@
+/*jslint node: true */
+
+'use strict';
+
+exports.getMovieGenres = function () {
+    return [
+        'All',
+        'Action',
+        'Adventure',
+        'Animation',
+        'Biography',
+        'Comedy',
+        'Crime',
+        'Documentary',
+        'Drama',
+        'Family',
+        'Fantasy',
+        'Film-Noir',
+        'History',
+        'Horror',
+        'Music',
+        'Musical',
+        'Mystery',
+        'Romance',
+        'Sci-Fi',
+        'Short',
+        'Sport',
+        'Thriller',
+        'War',
+        'Western'
+    ];
+};
+
+exports.getSerieGenres = function () {
+    return [
+        'All',
+        'Action',
+        'Adventure',
+        'Animation',
+        'Children',
+        'Comedy',
+        'Crime',
+        'Documentary',
+        'Drama',
+        'Family',
+        'Fantasy',
+        'Game Show',
+        'Home and Garden',
+        'Horror',
+        'Mini Series',
+        'Mystery',
+        'News',
+        'Reality',
+        'Romance',
+        'Science Fiction',
+        'Soap',
+        'Special Interest',
+        'Sport',
+        'Suspense',
+        'Talk Show',
+        'Thriller',
+        'Western'
+    ];
+};
