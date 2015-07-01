11f85d08175ec04ef0780428b0aa5f4fb6c40011
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-06-30 21:54:58 -0300

    favicon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/app/public/img/movieshark.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
